5ccf118b97d610c19bb6891d04d085f804959770
Pratama Julian Sufi Subhi <pratamaf1l4one@gmail.com>
2025-11-19 19:22:22 +0700

    Change database name from 'kopma' to 'db_kopma'

--- db.php
+++ db.php
@@ -5,7 +5,7 @@ session_start();
 $DB_HOST = "localhost";
 $DB_USER = "root";
 $DB_PASS = ""; // ganti jika diperlukan
-$DB_NAME = "kopma";
+$DB_NAME = "db_kopma";
 
 $conn = mysqli_connect($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
 if (!$conn) {
@@ -16,4 +16,5 @@ if (!$conn) {
 function e($s) {
     return htmlspecialchars($s, ENT_QUOTES, 'UTF-8');
 }
+
 ?>
